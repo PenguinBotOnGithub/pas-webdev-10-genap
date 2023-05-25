3546c1e4c7cacfd4d56eb48d32f6f01f7d8449d3
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-05-25 07:00:46 +0700

    Changed a comment

--- src/Components/AiringCard.js
+++ src/Components/AiringCard.js
@@ -16,8 +16,8 @@ function AiringCard({ title, studio, img, onSet, onDelete, hash, onPTW }) {
       <h3>{title}</h3>
       <h4>{studio}</h4>
       {
-        // For some reason this code below
-        // runs the onSet function??
+        // For some reason this button below
+        // runs the onSet function without being clicked first??
       }
       <button onClick={onSet(hash)} className="add-ptw airing-button">
         Add to Plan To Watch
