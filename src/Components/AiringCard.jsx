0424e96c9c698f45c5bbb8a721ed40e91c0f9a4d
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-05-29 14:56:54 +0700

    Show createdAt on every cards

--- src/Components/AiringCard.jsx
+++ src/Components/AiringCard.jsx
@@ -1,4 +1,4 @@
-function AiringCard({ title, studio, img, onSet, onDelete, id, onPTW }) {
+function AiringCard({ title, studio, img, onSet, onDelete, id, onPTW, createdAt }) {
   const button = !onPTW ? (
     <button onClick={() => onSet(id)} className="add-ptw ptw-button">
       Add to Plan To Watch
@@ -8,12 +8,14 @@ function AiringCard({ title, studio, img, onSet, onDelete, id, onPTW }) {
       Remove from Plan To Watch
     </button>
   );
+  const date = new Date(createdAt);
 
   return (
     <div className="airing-card">
       <img src={img} alt={title + "-alt"} />
       <h3>{title}</h3>
       <h4>{studio}</h4>
+      <h4>{date.toLocaleTimeString('en-ID')}</h4>
       {button}
     </div>
   );
--- src/Components/PTWCard.jsx
+++ src/Components/PTWCard.jsx
@@ -1,9 +1,13 @@
-function PTWCard({ title, studio, img, onDelete, id, onPTW }) {
+function PTWCard({ title, studio, img, onDelete, id, onPTW, createdAt }) {
+  const date = new Date(createdAt);
+
   return (
     <div className="ptw-card">
       <img src={img} alt={title + "-alt"} />
       <h3>{title}</h3>
       <h4>{studio}</h4>
+      <h4>{date.toLocaleTimeString()
+      }</h4>
       <button onClick={() => onDelete(id)} className="remove-ptw ptw-button">
         Remove from Plan To Watch
       </button>
